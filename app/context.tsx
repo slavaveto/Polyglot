76beaf7b38bb626916c025f0575c784cd9f0786e
me <slavaveto@gmail.com>
2025-12-19 17:04:14 -0300

    222

--- app/context.tsx
+++ app/context.tsx
@@ -4,7 +4,7 @@ import usePersistentState from "@/app/utils/usePersistentState"
 
 // ✅ Определяем типы
 interface MainContextType {
-
+//2345
     language: string;
     setLanguage: (lang: string) => void;
     selectedTab: string;
